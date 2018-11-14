aa8779fe2836df2643cce2f65a6dd72d050748f1
Prashant <44961024+jerry-143@users.noreply.github.com>
2018-11-14 14:27:42 +0530

    Update HlwApplication.java

--- Hlw/src/main/java/com/cg/Hlw/HlwApplication.java
+++ Hlw/src/main/java/com/cg/Hlw/HlwApplication.java
@@ -1,13 +1,9 @@
 package com.cg.Hlw;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class HlwApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HlwApplication.class, args);
+		
 		
 		System.out.println("Halo Prashant!!");
 	}
